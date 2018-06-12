0b8ec0040dc681bc1bc0c0e99329d9675345ce5a
Muhammad Qumail <muhammad.qumail1@gmail.com>
2018-06-12 16:40:07 +0200

    Committing project files

---
+++ app/src/main/java/app/com/muhammad/voice/MainActivity.java
@@ -0,0 +1,15 @@
+package app.com.muhammad.voice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
