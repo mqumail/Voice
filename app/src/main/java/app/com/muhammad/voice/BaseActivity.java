905a3629cc1f672e98695e47024442a690e7aad9
Muhammad Qumail <muhammad.qumail1@gmail.com>
2021-11-26 03:32:01 +0100

    Rename navigation drawer items

--- app/src/main/java/app/com/muhammad/voice/BaseActivity.java
+++ app/src/main/java/app/com/muhammad/voice/BaseActivity.java
@@ -1,19 +1,28 @@
 package app.com.muhammad.voice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import app.com.muhammad.voice.databinding.ActivityBaseBinding;
+import app.com.muhammad.voice.ui.openStreetMap.OsmFragment;
+import app.com.muhammad.voice.ui.profile.ProfileFragment;
+import app.com.muhammad.voice.ui.settings.SettingsFragment;
+import timber.log.Timber;
 
-public class BaseActivity extends AppCompatActivity
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     protected AppBarConfiguration appBarConfiguration;
     private ActivityBaseBinding binding;
@@ -25,6 +34,11 @@ public class BaseActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        // Timber Logger
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
 
         binding = ActivityBaseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -52,5 +66,53 @@ public class BaseActivity extends AppCompatActivity
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    /**
+     * Called when an item in the navigation menu is selected.
+     *
+     * @param item The selected item
+     * @return true to display the item as the selected item
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void displayView(int viewId) {
+        Fragment fragment = null;
+//        String title = getString(R.id.app_name);
+
+        switch (viewId) {
+            case R.id.nav_osm: {
+                //start home activity
+                Intent intent = new Intent(this, OsmFragment.class);
+                startActivity(intent);
+                Timber.d("Starting osm activity");
+                break;
+            }
+            case R.id.nav_profile: {
+                //start gallery
+                Intent intent = new Intent(this, ProfileFragment.class);
+                startActivity(intent);
+                Timber.d("Starting profile activity");
+                break;
+            }
+            case R.id.nav_settings: {
+                //start slideshow activity
+                Intent intent = new Intent(this, SettingsFragment.class);
+                startActivity(intent);
+                Timber.d("Starting settings activity");
+                break;
+            }
+        }
+
+        if (fragment != null) {
+//            FragmentTra
+        }
+
     }
 }
