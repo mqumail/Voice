da345786dcfe1baa44fb0d83a87b91d8396b1dd9
mqumail <muhammad_qumail@yahoo.com>
2022-04-06 04:50:46 +0200

    LoginActivity.java: added a function to save username (displayName) along with email to shared preferences ProfileFragment.java: clean up

--- app/src/main/java/app/com/muhammad/voice/LoginActivity.java
+++ app/src/main/java/app/com/muhammad/voice/LoginActivity.java
@@ -1,27 +1,31 @@
 package app.com.muhammad.voice;
 
+import static app.com.muhammad.voice.util.Constants.MY_PREFERENCES;
+import static app.com.muhammad.voice.util.Constants.USER_INFO_KEY;
+import static app.com.muhammad.voice.util.UiHelperMethods.hideSoftKeyboard;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import app.com.muhammad.voice.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity
 {
-    private static final String ACTIVITY_TAG = "LoginActivity";
+    int count = 1;
+    private static final String TAG = "LoginActivity";
 
     private ActivityLoginBinding binding;
 
@@ -49,73 +53,53 @@ public class LoginActivity extends AppCompatActivity
         login = binding.buttonLogin;
         register = binding.buttonRegister;
 
-        login.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                String email = binding.emailLoginText.getText().toString();
-                String password = binding.passwordLoginText.getText().toString();
-                signIn(email, password);
-            }
+        login.setOnClickListener(v -> {
+            hideSoftKeyboard(this);
+            String email = binding.emailLoginText.getText().toString();
+            String password = binding.passwordLoginText.getText().toString();
+            signIn(email, password);
         });
 
-        register.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent register = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(register);
-            }
+        register.setOnClickListener(v -> {
+            Intent register = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(register);
+            finish();
         });
+        Log.d(TAG, "onCreateView: Called - " + count + " times");
+        count = count+1;
     }
 
-    private void signIn(String email, String password)
-    {
-        Log.d(ACTIVITY_TAG, "SignIn: " + email);
-
-        if (!validateForm()) {
-            return;
-        }
-
+    private void signIn(String email, String password) {
+        Log.d(TAG, "SignIn: " + email);
+        if (!validateForm()) return; // Do not process before validating. TODO - need to validate all or use library
         // start auth process
-//        mAuth.signInWithEmailAndPassword(email, password)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information - store to SharedPreferences TODO
-//                            Log.d(ACTIVITY_TAG, "signInWithEmail:success");
-//                            FirebaseUser user = mAuth.getCurrentUser();
-//                            // add the above user info to SP - TODO
-//                            // continue to the home screen
-//                            Intent baseActivity = new Intent(LoginActivity.this, BaseActivity.class);
-//                            startActivity(baseActivity);
-//                            finish();
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w(ACTIVITY_TAG, "signInWithEmail:failure", task.getException());
-//                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        saveUserInfo(email, mAuth.getCurrentUser().getDisplayName());
+                        // Sign in success, update UI with the signed-in user's information - store to SharedPreferences TODO
+                        Log.d(TAG, "signInWithEmail:success: Signed in as - " + mAuth.getCurrentUser().getEmail());
+                        Intent login = new Intent(this, BaseActivity.class);
+                        startActivity(login);
+                        finish();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     // TODO: Move this to string utils
-    private boolean validateForm()
-    {
-        boolean valid = true;
-
+    private boolean validateForm() {
         String email = binding.emailLoginText.getText().toString();
+        boolean valid = true;
         if (TextUtils.isEmpty(email)) {
             binding.emailLoginText.setError("Required.");
             valid = false;
         } else {
             binding.emailLoginText.setError(null);
         }
-
         String password = binding.passwordLoginText.getText().toString();
         if (TextUtils.isEmpty(password)) {
             binding.passwordLoginText.setError("Required.");
@@ -123,7 +107,17 @@ public class LoginActivity extends AppCompatActivity
         } else {
             binding.passwordLoginText.setError(null);
         }
-
         return valid;
+    }
+
+    public void saveUserInfo(String email, String userName) {
+        Set<String> userInfo = new HashSet<>();
+        userInfo.add(email);
+        userInfo.add(userName);
+        SharedPreferences preferences = getSharedPreferences(MY_PREFERENCES, MODE_PRIVATE);
+        preferences.edit().putStringSet(USER_INFO_KEY,userInfo).apply();
+
+        Toast.makeText(this, "Login Successful! Continuing as: " + userName,
+                Toast.LENGTH_LONG).show();
     }
 }
