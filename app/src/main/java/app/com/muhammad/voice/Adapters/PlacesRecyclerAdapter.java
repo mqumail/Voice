3cfc273676e77420234cb75fc856c63e66b58c16
mqumail <muhammad_qumail@yahoo.com>
2022-04-06 04:58:23 +0200

    OsmFragment.java: -Added logic for the new views added. -Also dropping markers on the map in this class now. -Fixed permission issues which caused crashes. -now markers are showing correct and complete data from Firestore -When a marker is clicked, its check-in info is fetched and then displayed.
    PlacesRecyclerAdapter.java:
    -updated the list item so now the distance is shown from the user's location

--- app/src/main/java/app/com/muhammad/voice/Adapters/PlacesRecyclerAdapter.java
+++ app/src/main/java/app/com/muhammad/voice/Adapters/PlacesRecyclerAdapter.java
@@ -1,20 +1,35 @@
 package app.com.muhammad.voice.Adapters;
 
-import androidx.recyclerview.widget.RecyclerView;
+import static app.com.muhammad.voice.util.LocationHelper.distanceToFromCurrentLocation;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import app.com.muhammad.voice.DTO.PlaceInfo;
 import app.com.muhammad.voice.R;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>
+public class PlacesRecyclerAdapter extends RecyclerView.Adapter<PlacesRecyclerAdapter.RecyclerViewHolder>
 {
+    private List<PlaceInfo> mPlaces = new ArrayList<>();
+    private LayoutInflater mInflater;
+    private OnPlaceListener onPlaceListener;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public PlacesRecyclerAdapter(Context context, List<PlaceInfo> places, OnPlaceListener onPlaceListener) {
+        this.mInflater = LayoutInflater.from(context);
+        this.onPlaceListener = onPlaceListener;
+        mPlaces = places;
+    }
+
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
@@ -23,45 +38,38 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         // each data item is just a string in this case
         public TextView poiName;
         public TextView poiAddress;
-        public ItemClickListener itemClickListener;
+        public TextView poiDistance;
+        public OnPlaceListener onPlaceListener;
 
-        public RecyclerViewHolder(View v, ItemClickListener itemClickListener)
+        public RecyclerViewHolder(View v, OnPlaceListener onPlaceListener)
         {
             super(v);
             poiName = v.findViewById(R.id.poi_name);
             poiAddress = v.findViewById(R.id.poi_address);
-            this.itemClickListener = itemClickListener;
+            poiDistance = v.findViewById(R.id.poi_distance);
+            this.onPlaceListener = onPlaceListener;
 
             v.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            itemClickListener.onItemClick(view, getBindingAdapterPosition());
+            onPlaceListener.onPlaceClick(view, getBindingAdapterPosition());
         }
     }
 
     // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
+    public interface OnPlaceListener {
+        void onPlaceClick(View view, int position);
     }
 
-    private List<PlaceInfo> mPlaces;
-    private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public RecyclerViewAdapter(Context context, List<PlaceInfo> places, ItemClickListener itemClickListener) {
-        this.mInflater = LayoutInflater.from(context);
-        this.mClickListener = itemClickListener;
-        mPlaces = places;
-    }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public RecyclerViewAdapter.RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public PlacesRecyclerAdapter.RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.poi_detail, parent, false);
-        return new RecyclerViewHolder(view, mClickListener);
+        return new RecyclerViewHolder(view, onPlaceListener);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -69,6 +77,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public void onBindViewHolder(RecyclerViewHolder holder, int position) {
         holder.poiName.setText(mPlaces.get(position).getName());
         holder.poiAddress.setText(mPlaces.get(position).getAddress());
+        double distance = distanceToFromCurrentLocation(mPlaces.get(position).getLatLng(), mInflater.getContext());
+        double distanceInKM = 0.001 * distance;
+
+        String formattedDistance = new DecimalFormat("#.##").format(distanceInKM);
+
+        holder.poiDistance.setText(formattedDistance + " KM away");
     }
 
     // Return the size of your dataset (invoked by the layout manager)
